9050810c86b3ac24961cc6eff89e938d2d844be1
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-08-31 17:19:58 +0500

    Update user_input.py

--- modules/user_input.py
+++ modules/user_input.py
@@ -1,1 +1,15 @@
-# Mechanism to input list of usernames of interest 
+# modules/user_input.py
+
+def get_users_of_interest():
+    """
+    Collects a list of usernames of interest from the user.
+    """
+    users = []
+    print("Enter usernames of interest (type 'done' when finished):")
+    while True:
+        username = input()
+        if username.lower() == 'done':
+            break
+        users.append(username)
+    return users
+ 
