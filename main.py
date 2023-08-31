c37bcd1224af987a7e9048019f3302ef28d771e8
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-08-31 17:24:27 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-# Main script to run the bot 
+# main.py
+
+from modules.user_input import get_users_of_interest
+
+def main():
+    users_of_interest = get_users_of_interest()
+    print(f"Monitoring for users: {', '.join(users_of_interest)}")
+
+if __name__ == "__main__":
+    main()
