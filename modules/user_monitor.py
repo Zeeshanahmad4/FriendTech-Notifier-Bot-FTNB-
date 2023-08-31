1af5026ebb3286b98db0acd93f98c0a0c94c531f
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-08-31 17:20:25 +0500

    Update user_monitor.py

--- modules/user_monitor.py
+++ modules/user_monitor.py
@@ -1,1 +1,11 @@
-# Monitors the app for new user registrations 
+# modules/user_monitor.py
+
+from modules.notification import send_notification
+
+def check_new_user(username, users_of_interest):
+    """
+    Checks if a new user is in the list of users of interest.
+    If yes, sends a notification.
+    """
+    if username in users_of_interest:
+        send_notification(username)
