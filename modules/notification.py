1b4a50c571076b481b01ea0396b42316ec033fda
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-08-31 17:18:57 +0500

    Update notification.py

--- modules/notification.py
+++ modules/notification.py
@@ -1,1 +1,9 @@
-# Handles the notification sending mechanism 
+# modules/notification.py
+
+def send_notification(username):
+    """
+    Sends a notification when a specific user joins.
+    For this example, we'll print to the console.
+    """
+    print(f"Notification: User {username} has joined friend.tech!")
+
